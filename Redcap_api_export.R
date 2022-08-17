7e04eb185324da91b7381d0fa3fe9292f0924531
Alexey Gilman <Alexey.Gilman@doh.wa.gov>
2022-08-16 21:28:38 -0700

    simplify export example

--- Redcap_api_export.R
+++ Redcap_api_export.R
@@ -1,14 +1,15 @@
 library(httr)
 
 url <- 'https://redcap.doh.wa.gov/api/'
+key <- '32_character_API_token_string'
 
-formData <- list(token='',
+formData <- list(token=key,
                  content='record',
                  format='csv',
                  returnFormat='csv',
                  rawOrLabel = 'raw'
 )
 
-data <- httr::POST(url, body = formData, encode = "form")
-data <- httr::content(data)
+full_response <- httr::POST(url, body = formData)
+data <- httr::content(full_response)
 
